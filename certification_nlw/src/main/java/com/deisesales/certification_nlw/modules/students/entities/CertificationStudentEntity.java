8fd9edf235fa838abadc34a3c0e210cf1bd094b3
deise <deisesalles18@gmail.com>
2024-02-10 23:52:21 -0300

    2.2 correção das anotações
    correção das anotações porque ao rodar o spring tava dando erro acusando duplicidade na criação das tabelas

--- certification_nlw/src/main/java/com/deisesales/certification_nlw/modules/students/entities/CertificationStudentEntity.java
+++ certification_nlw/src/main/java/com/deisesales/certification_nlw/modules/students/entities/CertificationStudentEntity.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -24,13 +26,18 @@ public class CertificationStudentEntity {
     @Column(length = 10)
     private int grate;
 
-    @JoinColumn(name = "student_id")
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
     private UUID studentID;
 
     @ManyToOne
     @JoinColumn(name = "student_id", insertable = false, updatable = false)
     private StudentEntity studentEntity;
 
-    //private List<AnswersCertificationsEntity> answersCertificationsEntities;
+    @OneToMany
+    @JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
+    private List<AnswersCertificationsEntity> answersCertificationsEntities;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
