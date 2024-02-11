58c122a99d13ea01f23825b994440500072a050e
deise <deisesalles18@gmail.com>
2024-02-11 00:14:53 -0300

    2.3 pacote repositories
    Criação do pacote repositories, e da classe CertificationStudentEntityRepository para fazer validação no meu database

---
+++ certification_nlw/src/main/java/com/deisesales/certification_nlw/modules/students/repositories/CertificationStudentEntityRepository.java
@@ -0,0 +1,13 @@
+package com.deisesales.certification_nlw.modules.students.repositories;
+
+import com.deisesales.certification_nlw.modules.students.entities.CertificationStudentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CertificationStudentEntityRepository extends JpaRepository<CertificationStudentEntity, UUID> {
+    @Query("SELECT c FROM certifications INNER JOIN c.studentEntity std WHERE std.email = :email AND c.technology = :technology")
+    List<CertificationStudentEntity> findByStudentEmailAndTechnology(String email, String technology);
+}
--- certification_nlw/src/main/java/com/deisesales/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ certification_nlw/src/main/java/com/deisesales/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,15 +1,23 @@
 package com.deisesales.certification_nlw.modules.students.useCases;
 
 import com.deisesales.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.deisesales.certification_nlw.modules.students.entities.CertificationStudentEntity;
+import com.deisesales.certification_nlw.modules.students.repositories.CertificationStudentEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
+    @Autowired
+    private CertificationStudentEntityRepository certificationStudentEntityRepository;
+
     public boolean execute(VerifyHasCertificationDTO dto) {
 
-        Object object = "deisesales18@gmail.com";
-        if(dto.getEmail().equals(object) && dto.getTechnology().equalsIgnoreCase("JAVA")) {
+        var result = this.certificationStudentEntityRepository.findByStudentEmailAndTechnology(dto.getEmail(),
+                dto.getTechnology());
+
+        if(!result.isEmpty()) {
             return true;
         }
         return false;
